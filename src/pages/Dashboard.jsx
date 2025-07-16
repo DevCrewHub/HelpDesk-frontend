5ec982733d18b26b990f50583176b87013fea736
shrey <shreyasshenoy601@gmail.com>
2025-07-16 12:11:06 +0530

    Added loading states

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ const Dashboard = () => {
   const role = localStorage.getItem("userRole");
   const isAgent = role === "AGENT";
   const isCustomer = role === "CUSTOMER";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchtickets();
@@ -14,11 +15,14 @@ const Dashboard = () => {
 
   const fetchtickets = async () => {
     try {
+      setLoading(true);
       const url = isAgent ? "/agent/assigned/tickets" : isCustomer ? "/customer/ticketsCreated" : "/admin/tickets";
       const response = await api.get(url);
       setTickets(response.data);
     } catch (error) {
       console.error("Error fetching ticket tickets:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -52,18 +56,20 @@ const Dashboard = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-4">
-          {items.map((item, index) => (
-            <div
-              key={index}
-              className="bg-white border border-gray-200 shadow-sm rounded-xl p-4 flex flex-col items-center justify-center hover:shadow-md transition-shadow"
-            >
-              <div className="mb-2">{item.icon}</div>
-              <div className="text-3xl font-semibold text-gray-800">{item.count}</div>
-              <div className="mt-2 text-gray-500 text-sm text-center">{item.label}</div>
-            </div>
-          ))}
-        </div>
+        {loading ? <div className="h-6 w-6 mt-2 rounded-full border-4 border-gray-300 border-t-indigo-600 animate-spin"></div> :
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-4">
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className="bg-white border border-gray-200 shadow-sm rounded-xl p-4 flex flex-col items-center justify-center hover:shadow-md transition-shadow"
+              >
+                <div className="mb-2">{item.icon}</div>
+                <div className="text-3xl font-semibold text-gray-800">{item.count}</div>
+                <div className="mt-2 text-gray-500 text-sm text-center">{item.label}</div>
+              </div>
+            ))}
+          </div>
+        }
 
       </div>
   );
